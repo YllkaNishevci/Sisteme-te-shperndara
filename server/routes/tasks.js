8f161008549755b82bbe97e6df2052c156adfcd5
Festinë Bytyçi <lirone.bytyci@student.uni-pr.edu>
2024-04-12 18:12:27 +0200

    krijimi i routes tasks.js, get all tasks

---
+++ server/routes/tasks.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../model/tasks");
+
+// Get all tasks
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
