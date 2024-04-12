a3df46bf7a6d854775569f1bcd5e108b626bab1d
Festinë Bytyçi <lirone.bytyci@student.uni-pr.edu>
2024-04-12 18:15:20 +0200

    get specific task by id

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,12 +4,30 @@ const Task = require("../model/tasks");
 
 // Get all tasks
 router.get("/", async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.json(tasks);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
+// Get a specific task
+router.get("/:id", getTask, (req, res) => {
+    res.json(res.task);
+});
+
+async function getTask(req, res, next) {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (task == null) {
+            return res.status(404).json({ message: "Cannot find task" });
+        }
+        res.task = task;
+        next();
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
 
 module.exports = router;
